792b8dd7c59ca15ae4f716e456c82a19e8861eec
Jordan Hammond <jahammo2@gmail.com>
2016-09-20 17:40:58 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname,
+    filename: "dist/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
